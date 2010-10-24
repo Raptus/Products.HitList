df48d1355ba1c72e53f275d23e4e8fe09e03a05e
Simon Käser <skaeser@raptus.com>
2010-10-24 12:57:33 +0000

    set zip_safe flag to False and prepared for release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0b1'
+version = '2.0b2'
 
 setup(name='Products.HitList',
       version=version,
@@ -21,7 +21,7 @@ setup(name='Products.HitList',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
