61f92ce7c6351136be3f44738a1859672840d5d0
Jonas Wagner <jwagner@raptus.com>
2012-11-02 10:15:52 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '2.0b4'
 setup(name='Products.HitList',
       version=version,
       description="Provides a personal hit-list which may be displayed in a portlet or a view",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='Products.HitList',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/Products.HitList',
+      url='https://github.com/Raptus/Products.HitList',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
